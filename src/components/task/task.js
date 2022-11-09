c8bc055fd3547e83dd704402f4868758a5921cef
VitalkaZu <vitalkazu@gmail.com>
2022-11-09 23:29:15 +0800

    09.11.2022 HOME

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,24 +1,56 @@
 import React from "react";
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
-const Task = ({label, publicdate}) => {
-    const  timeDistance = formatDistanceToNow(
-        publicdate,
-        {addSuffix: true}
-    )
+export default class Task extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
 
-    return (
-        <div className="view">
-                <input className="toggle" type="checkbox"/>
-                    <label>
-                        <span className="description">{label}</span>
-                        <span className="created">{timeDistance}</span>
-                    </label>
-                    <button className="icon icon-edit"></button>
-                    <button className="icon icon-destroy"></button>
-        </div>
-    )
+    taskComplete = () => {
+        console.log("Done "  + this.props.label)
+    }
 
+    render() {
+        const {label, publicdate} = this.props
+
+        const  timeDistance = formatDistanceToNow(
+            publicdate,
+            {addSuffix: true}
+        )
+
+        return (
+            <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>
+                    <span className="description" onClick={this.taskComplete}>{label}</span>
+                    <span className="created">{timeDistance}</span>
+                </label>
+                <button className="icon icon-edit"></button>
+                <button className="icon icon-destroy"></button>
+            </div>
+        )
+    }
 }
 
-export default Task;
+
+// const Task = ({label, publicdate}) => {
+//     const  timeDistance = formatDistanceToNow(
+//         publicdate,
+//         {addSuffix: true}
+//     )
+//
+//     return (
+//         <div className="view">
+//                 <input className="toggle" type="checkbox"/>
+//                     <label>
+//                         <span className="description">{label}</span>
+//                         <span className="created">{timeDistance}</span>
+//                     </label>
+//                     <button className="icon icon-edit"></button>
+//                     <button className="icon icon-destroy"></button>
+//         </div>
+//     )
+//
+// }
+//
+// export default Task;
